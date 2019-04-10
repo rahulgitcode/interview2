1f94ddbb1673c621eaa58f3b2ce4717b65be754c
Bhoj <e005391@hms.com>
2019-04-10 15:44:55 -0500

    added more code

--- src/com/hmsy/dailycodingchallenge/MaxSumOfNonAdjacentNumbers.java
+++ src/com/hmsy/dailycodingchallenge/MaxSumOfNonAdjacentNumbers.java
@@ -8,6 +8,8 @@ Given a list of integers, write a function that returns the largest sum of non-a
 
 For example, [2, 4, 6, 2, 5] should return 13, since we pick 2, 6, and 5. [5, 1, 1, 5] should return 10, since we pick 5 and 5.
 
+Reference:  https://www.youtube.com/watch?v=UtGtF6nc35g
+
 Follow-up: Can you do this in O(N) time and constant space?
  * 
  * @author e005391
